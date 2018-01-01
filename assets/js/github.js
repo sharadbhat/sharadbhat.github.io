a02ca7bd278624877928fd9b137fccaeae258230
Sharad Bhat <sharad.mbhat@gmail.com>
2018-01-02 00:03:30 +0530

    Added contributions

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,14 +1,15 @@
 url1 = "https://api.github.com/repos/sharadbhat/VideoHub";
 url2 = "https://api.github.com/repos/sharadbhat/Zomatopy";
 url3 = "https://api.github.com/repos/sharadbhat/Recommender-Engine-API";
+url4 = "https://api.github.com/repos/OmkarPathak/pygorithm"
 
-url = [url1, url2, url3];
+url = [url1, url2, url3, url4];
 
 var star = 'star'
 var fork = 'fork'
 
 var xhr = new XMLHttpRequest();
-for (var i = 1; i < 4; i++) {
+for (var i = 1; i < url.length + 1; i++) {
   xhr.open("GET", url[i - 1], false);
   xhr.send();
 
